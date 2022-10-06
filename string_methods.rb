79cee1baddc22b21304121d8694bb26b3f165fd4
Miryam Bathilde <mbathilde@gmail.com>
2022-10-06 15:33:43 +0200

    string methods

---
+++ string_methods.rb
@@ -0,0 +1,33 @@
+
+name = 'Working with string methods'
+
+# string length
+puts name.length # 27
+
+# string reverse
+puts name.reverse # sdohtem gnirts htiw gnikroW
+
+# string upcase
+puts name.upcase # WORKING WITH STRING METHODS
+
+# string downcase
+puts name.downcase # working with string methods
+
+# string capitalize
+puts name.capitalize # Working with string methods
+
+# string swapcase (is the opposite of capitalize)
+puts name.swapcase  # wORKING WITH STRING METHODS
+
+# string include?
+puts name.include?('with') # true
+
+# string start_with?
+puts name.start_with?('Working') # true
+
+# string end_with?
+puts name.end_with?('methods') # true
+
+# string nil?
+puts name.nil? # false
+
