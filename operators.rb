fb2e5259f586203945d184331f5e765ee9791866
Miryam Bathilde <mbathilde@gmail.com>
2022-10-06 14:46:16 +0200

    arithmetic operators

---
+++ operators.rb
@@ -0,0 +1,21 @@
+
+number1 = 10.0
+number2 = 20.0
+
+p number1, number2
+
+sum = number1 + number2
+puts 'The result of the sum is: '
+puts sum
+
+extract = number1 - number2
+puts 'The result of the extract is: '
+puts extract
+
+multiplication = number1 * number2
+puts 'The result of the multiplication is: '
+puts multiplication
+
+division = number1 / number2
+puts 'The result of the division is: '
+puts division
