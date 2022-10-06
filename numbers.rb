3bd63c8f51e0ed8792100a5bf01adc6ca5a0ecd1
Miryam Bathilde <mbathilde@gmail.com>
2022-10-06 14:16:13 +0200

    numbers

---
+++ numbers.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# integer numbers
+puts 5 + 5
+puts 10 - 4
+puts 5 * 5
+puts 100 / 10
+
+# float numbers
+puts 3.5 + 3.5
+puts 10.5 - 4.5
+puts 5.5 * 5.5
+puts 20.5 / 10.5
