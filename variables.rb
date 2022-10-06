1dd0abeff185a97e97a5f845441169ebb77201cd
Miryam Bathilde <mbathilde@gmail.com>
2022-10-06 15:00:29 +0200

    combining variables and strings

--- variables.rb
+++ variables.rb
@@ -5,3 +5,15 @@ puts number
 
 name = 'Miryam'
 puts name
+
+#  variables and strings
+
+string = 'Course of '
+string2 = 'Ruby'
+
+puts "Hola #{string} #{string2}"
+puts string + string2 # => Course of Ruby
+
+string3 = ' from scratch'
+
+puts string + string2 + string3 # => Course of Ruby from scratch
