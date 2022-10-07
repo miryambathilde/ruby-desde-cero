43f4b90037f2b917ad0f9755bf6b20a3e4e79c48
Miryam Bathilde <mbathilde@gmail.com>
2022-10-07 12:02:40 +0200

    in ruby all is an object

---
+++ objects.rb
@@ -0,0 +1,11 @@
+# in Ruby all is an object
+name = 'Miryam Bathilde'
+number = 34
+
+p name.length # => 15
+p name.class # => String
+p number.class # => Integer
+
+5.times { puts 'Ruby' } # => Hello Hello Hello Hello Hello
+
+p 4.odd? # => false
