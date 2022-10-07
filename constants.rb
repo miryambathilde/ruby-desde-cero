daca9c2c45a9862e74a09c580f0344f4b8c2de04
Miryam Bathilde <mbathilde@gmail.com>
2022-10-07 10:38:24 +0200

    constants

---
+++ constants.rb
@@ -0,0 +1,22 @@
+
+PI = 3.1416
+PI = 3.14169
+
+puts PI
+
+=begin
+Output:
+3.14169
+C:/repositorios ruby/ruby desde cero/constants.rb:3: warning: already initialized constant PI
+C:/repositorios ruby/ruby desde cero/constants.rb:2: warning: previous definition of PI was here
+=end
+
+salary = 1000
+
+IRPF = 0.16
+
+puts salary * IRPF
+
+netSalary = salary - (salary * IRPF)
+
+puts netSalary # => 840.0
