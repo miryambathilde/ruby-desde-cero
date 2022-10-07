1fffde552792f85de014b1473edba17eeedc901c
Miryam Bathilde <mbathilde@gmail.com>
2022-10-07 12:58:51 +0200

    optional and variables args or params

---
+++ opcional_args.rb
@@ -0,0 +1,13 @@
+# optional params or args, must be after the required params, always to right of them, is a best practice
+
+def name(name1, name2, *name3)
+  puts "The name one is #{name1}"
+  puts "The name two is #{name2}"
+  puts "The name three is #{name3}"
+end
+
+name("Miryam", "Luca")
+
+# The name one is Miryam
+# The name two is Luca
+# The name three is []
