e29453b076f90245e66555ce530ba47fb4bdb2d8
Miryam Bathilde <mbathilde@gmail.com>
2022-10-07 10:25:42 +0200

    instruction delimiters

---
+++ instruction_delimiters.rb
@@ -0,0 +1,10 @@
+
+num1 = 10; num2 = 20; num3 = 30 # semicolon delimiter
+
+puts num1 # 10
+
+name = "Hello I'm Miryam from Ruby\nand I'm learning Ruby"
+
+puts name
+# Hello I'm Miryam from Ruby
+# and I'm learning Ruby
